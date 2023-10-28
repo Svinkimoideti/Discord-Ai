33c44393a5340665a8557bffefb2c48e8b7ce685
Svinkimoideti <132742220+Svinkimoideti@users.noreply.github.com>
2023-10-28 13:17:05 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+
+import discord
+
+from discord.ext import commands
+
+from model import AI
+
+intents = discord.Intents.default()
+
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+
+async def on_ready():
+  
+    print(f'Все работает')
+
+@bot.command()
+async def image(ctx):
+    if len(ctx.message.attachments) > 0:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            file_url = attachment.url
+            await attachment.save(f'./images/{file_name}')
+            await ctx.send('Я украл и сохранил твою картинку))')
+            class_name = AI(f'./images/{file_name}')
+            await ctx.send (class_name)
+    else:
+        await ctx.send('А где фото?((')
+
+bot.run("MTE2NTE5MDE2ODI1MzMyMTIxNg.GB5wlI.CUvu2dS2JRNMpP0qXDrxFbbzdsjt89aoH6kjjU")
